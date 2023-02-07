de86ad25964fb6a6718379e9f9ca714360b3f861
othernedwin <edwinlwhitman@icloud.com>
2023-02-07 14:04:39 -0800

    Added fields to user registered event event
    [src/events/EventUserRegistered.go]
    - Add additional fields to the `USER_REGISTERED` event: `firstName`, `lastName`, `email`, and `phoneNumber`

--- src/events/EventUserRegistered.go
+++ src/events/EventUserRegistered.go
@@ -4,11 +4,15 @@ import (
 	"time"
 )
 
-func NewEventUserRegistered(userID string, metadata map[string]interface{}) *Event {
+func NewEventUserRegistered(userID string, firstName string, lastName string, email string, phoneNumber string, metadata map[string]interface{}) *Event {
 	return &Event{
 		EventType: "USER_REGISTERED",
 		Data: map[string]interface{}{
-			"userID": userID,
+			"userID":      userID,
+			"firstName":   firstName,
+			"lastName":    lastName,
+			"email":       email,
+			"phoneNumber": phoneNumber,
 		},
 		Timestamp: time.Now().UTC(),
 		Metadata:  metadata,
